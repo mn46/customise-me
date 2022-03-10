2f080c4769a59fdbbb3d62a1649f9256a01b5f02
vterziyski <vasi0757@stud.kea.dk>
2022-03-10 13:19:41 +0100

    homepage

--- home.js
+++ home.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".hamburgermenu");
 const navMenu = document.querySelector("#navigation_list");
 
+
 hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('show');
 });
