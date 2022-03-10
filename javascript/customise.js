7e07c97d300e6f39edea8387acde9a3340edd746
mn46 <m.nowak033@gmail.com>
2022-03-10 13:09:40 +0100

    almost finished customise page

--- javascript/customise.js
+++ javascript/customise.js
@@ -15,7 +15,7 @@ fetch(url, options)
 })
 .then((data) => {
     //We have the data
-    console.log(data);
+    //console.log(data);
     getData(data);
 })
 .catch((e) => {
@@ -31,19 +31,19 @@ function getData(data) {
 
 function addTemplate(oils) {
 
-    if (oils.note = "") {
+    if (oils.note === "") {
         return;
     }
 
     // template for top note
 
-    if (oils.note = "headnote") {
+    if (oils.note === "headnote") {
         const topTemplate = document.querySelector("#top_note_oil").content;
         const topClone = topTemplate.cloneNode(true);
 
         // content
 
-        topClone.querySelector(".oil_info img").src = "";
+        topClone.querySelector(".oil_info img").src = `css/assets/oils/${oils.img}`;
         topClone.querySelector(".oil_info h4").textContent = oils.name;
         topClone.querySelector(".oil_info p").textContent = oils.description;
 
@@ -52,29 +52,100 @@ function addTemplate(oils) {
         document.querySelector(".container_oils_top").appendChild(topClone);
     }
 
+    // template for middle note
+
+    if (oils.note === "heartnote") {
+        const middleTemplate = document.querySelector("#middle_note_oil").content;
+        const middleClone = middleTemplate.cloneNode(true);
+
+        // content
+
+        middleClone.querySelector(".oil_info img").src = `css/assets/oils/${oils.img}`;
+        middleClone.querySelector(".oil_info h4").textContent = oils.name;
+        middleClone.querySelector(".oil_info p").textContent = oils.description;
+
+        // adding to parent
+
+        document.querySelector(".container_oils_middle").appendChild(middleClone);
+    }
+
+    // template for base note
+
+    if (oils.note === "basenote") {
+        const baseTemplate = document.querySelector("#base_note_oil").content;
+        const baseClone = baseTemplate.cloneNode(true);
+
+        // content
+
+        baseClone.querySelector(".oil_info img").src = `css/assets/oils/${oils.img}`;
+        baseClone.querySelector(".oil_info h4").textContent = oils.name;
+        baseClone.querySelector(".oil_info p").textContent = oils.description;
+
+        // adding to parent
+
+        document.querySelector(".container_oils_base").appendChild(baseClone);
+    }
+
+}
+
+// elements appearing on the left
+
+let heart = document.querySelector("#heart_icon");
+let cedar = document.querySelector("#cedar_icon");
+let patchouli = document.querySelector("#patchouli_icon");
+let rosemary = document.querySelector("#rosemary_icon");
+let price = document.querySelector("#grid_left #price_container p");
+let sum = 0;
+
+displayHeart();
+displayRosemary();
+displayPatchouli();
+displayCedar();
+
+function hideText() {
+    document.querySelector("#start_creating h2").classList.add("hidden");
+}
+
+function showHeart() {
+    heart.classList.remove("hidden");
+}
+
+function showCedar() {
+    cedar.classList.remove("hidden");
+}
+
+function showPatchouli() {
+    patchouli.classList.remove("hidden");
+}
+
+function showRosemary() {
+    rosemary.classList.remove("hidden");
+}
+
+function displayCedar() {
+    document.querySelector(".container_oils_base").addEventListener("click", showCedar);
+    addPrice();
+}
+
+function displayPatchouli() {
+    document.querySelector(".container_oils_middle").addEventListener("click", showPatchouli);
+    addPrice();
+}
+
+function displayRosemary() {
+    document.querySelector(".container_oils_top").addEventListener("click", showRosemary);
+    addPrice();
+}
+
+function displayHeart() {
+    document.querySelector("#shapes .color_icon").addEventListener("click", showHeart);
+    document.querySelector("#shapes .color_icon").addEventListener("click", hideText);
+    addPrice();
+}
+
+function addPrice() {
+    let newSum = sum + 10;
+    price.textContent = "DKK" + newSum;
 }
 
 
-// behavior of icons
-
-function changeAppearance() {
-
-    document.querySelector(".vis_container .color_icon").addEventListener("click", swapImage);
-
-    function swapImage() {
-        document.querySelector(".vis_container .color_icon .first").classList.add("hidden");
-        document.querySelector(".vis_container .color_icon img:last-child").classList.remove("hidden");
-        document.querySelector(".vis_container .color_icon p").classList.add("color");
-        document.querySelector(".vis_container .color_icon").addEventListener("click", unclick);
-    }
-
-    function unclick() {
-        document.querySelector(".vis_container .color_icon .first").classList.remove("hidden");
-        document.querySelector(".vis_container .color_icon img:last-child").classList.add("hidden");
-        document.querySelector(".vis_container .color_icon p").classList.remove("color");
-        
-        document.querySelector(".vis_container .color_icon").addEventListener("click", swapImage);
-    }
-}
-
-changeAppearance();
